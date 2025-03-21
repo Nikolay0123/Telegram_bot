bb98fe23bf4631c770d5e8642036b86b09d591d1
skillbox <hdhdkfh@dd>
2025-03-21 20:50:58 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,48 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from aiogram.enums.dice_emoji import DiceEmoji
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="7999093710:AAFCGSP_tK3L6YaZNcoGZ2AEGsl-Gnxp7SM")
+# Диспетчер
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+@dp.message(Command("test1"))
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+
+@dp.message(Command("answer"))
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+
+@dp.message(Command("reply"))
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+
+
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    dp.message.register(cmd_test2, Command("test2"))
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
