0632f1a2340bd58086de48f7c6ab654e227230d2
Nikolay <izosimovnicolay01@gmail.com>
2025-03-23 19:53:47 +0300

    23.03.2025

--- bot.py
+++ bot.py
@@ -3,13 +3,39 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.enums.dice_emoji import DiceEmoji
+from datetime import datetime
+from config_reader import config
+from aiogram import F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
-bot = Bot(token="7999093710:AAFCGSP_tK3L6YaZNcoGZ2AEGsl-Gnxp7SM")
+bot = Bot(token=config.bot_token.get_secret_value(), default=DefaultBotProperties(
+        parse_mode=ParseMode.MARKDOWN_V2))
 # Диспетчер
 dp = Dispatcher()
+dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+
+
+@dp.message(Command("add_to_list"))
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(Command("show_list"))
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущен {started_at}")
 
 
 # Хэндлер на команду /start
@@ -36,13 +62,23 @@ async def cmd_reply(message: types.Message):
 async def cmd_test2(message: types.Message):
     await message.reply("Test 2")
 
+@dp.message(F.text, Command("test"))
+async def any_message(message: Message):
+    await message.answer(
+        "Hello, *world*\!",
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     dp.message.register(cmd_test2, Command("test2"))
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, mylist=[1, 2, 3])
+    # await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     asyncio.run(main())
+
+# 7999093710: AAFCGSP_tK3L6YaZNcoGZ2AEGsl - Gnxp7SM
 
